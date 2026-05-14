46151ddef05ed3e29e1ee701784beb6859175f62
coddrago <drakongg1337@gmail.com>
2026-05-14 14:20:16 +0000

    очередной фикс db writers

--- heroku/database.py
+++ heroku/database.py
@@ -54,7 +54,10 @@ __all__ = [
 
 logger = logging.getLogger(__name__)
 _DB_PROTECTED_OWNERS = {"HerokuPluginSecurity"}
-_DB_ALLOWED_WRITERS = {f"{__package__}.modules.heroku_plugin_security"}
+_DB_ALLOWED_WRITERS = {
+    f"{__package__}.loader",
+    f"{__package__}.modules.heroku_plugin_security",
+}
 
 
 class NoAssetsChannel(Exception):
